ad0a1c8f807c4ce08aa656621a9d8fd4f0f824ca
Gregory Grillone <gjg73@yahoo.com>
2012-02-06 15:19:08 -0500

    Added links and styles for links in the nav bar

--- application.js
+++ application.js
@@ -12,4 +12,18 @@ $(document).ready(function() {
       opacity: 0.08,
       monochrome: false
   });
+  
+	/* Draws triangle (above selected href in .nav) for canvas element */
+	var c=document.getElementById("link-selected");
+	var ctx=c.getContext("2d");
+	ctx.fillStyle="#FF0000";
+	ctx.strokeStyle="#FF0000";
+
+	ctx.moveTo(10,10);
+	ctx.lineTo(40,10);
+	ctx.lineTo(25,30);
+	ctx.lineTo(10,10);
+
+	ctx.fill();
+	ctx.stroke();
 });
